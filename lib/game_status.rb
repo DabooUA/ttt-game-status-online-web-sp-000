4f1fa36259112bb387f496988fa051392c2351ea
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-05 00:56:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,6 +33,7 @@ def won?
       return win_combination # return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
+    end
 }
     else
       false
