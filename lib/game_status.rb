9b9ea4a53faad25f53d88fb07f11d5b7f43affe1
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-05 01:46:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,8 +32,9 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
      return win_combination # return the win_combination indexes that won.
-    else
-      false
+
       end
     end
+  else
+    false
   end
