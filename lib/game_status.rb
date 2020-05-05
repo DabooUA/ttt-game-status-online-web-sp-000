2375d88cb8fdbb7b9b4f32be844e4d73d0514c0b
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-05 01:16:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 [2,4,6]  # Right diagnol
 ]
 def won?
-  for WIN_COMBINATIONS.each |win_combination|
+   WIN_COMBINATIONS.each |win_combination|
 
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     # grab each index from the win_combination that composes a win.
