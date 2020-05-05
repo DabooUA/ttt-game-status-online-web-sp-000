e7b768b92fd39dd67fadf1057d4888452734c883
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-05 02:11:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ def won?(board)
 
   def full?(board)
     board.all? do |cell|
-      if cell == "X" || BOX == "O"
+      if cell == "X" || cell == "O"
        return true
       else
        false
